a1dcfff71d58fb518548bce38a697ac957ce1c3a
LQuangTien <tienkg5554@gmail.com>
2022-04-11 16:47:11 +0700

    add swagger

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const client = require("../db");
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    // info: { title: "Search engine API", version: "1.0.0" },
+    openapi: "3.0.0",
+    info: {
+      title: "Express API for JSONPlaceholder",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/routes.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(cors());
+app.use("/api", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const getindicesData = require("../controllers/getIndexdata");
 const insertApi = require("../controllers/registration");
@@ -20,7 +39,6 @@ app.get("/", async (req, res) => {
     //  const result = await client.search({
     //    index: "blog",
     //  });
-    console.log(result);
     res.send({
       status: 200,
       message: "API is working fine!",
@@ -30,13 +48,91 @@ app.get("/", async (req, res) => {
   }
 });
 
-// // Get elastic indices data
+/**
+ * @swagger
+ * paths:
+ *   /search/alldata/{index}:
+ *    get:
+ *      summary: Get all data of index
+ *      parameters:
+ *        - in: path
+ *          name: index
+ *          schema:
+ *            type: string
+ *            required: true
+ *            description: index name
+ *      responses:
+ *        200:
+ *          description: Success
+ */
 app.get("/search/alldata/:index", getindicesData.getEachIndicesData);
 
-// Get single elastic indices data
+/**
+ * @swagger
+ * /search/single/data:
+ *   post:
+ *     summary: Search a document by query.
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 index:
+ *                   type: string
+ *                   example: blog
+ *                 match:
+ *                   type: object
+ *                   properties:
+ *                     query:
+ *                       type: object
+ *                       description: query option.
+ *                       example: {
+ *                          title: "blog",
+ *                       }
+ */
 app.post("/search/single/data", getindicesData.getEachIndicesSingleRecord);
 
-// Insert single data into Elastic-search directly // indices students
+/**
+ * @swagger
+ * /insert/single/data:
+ *   post:
+ *     summary: Add a single data.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *               type: object
+ *               properties:
+ *                 index:
+ *                   type: string
+ *                   example: blog
+ *                 title:
+ *                   type: string
+ *                   example: blog a
+ *                 content:
+ *                   type: string
+ *                   example: content a
+ *     responses:
+ *       201:
+ *         description: created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 index:
+ *                   type: string
+ *                   example: blog
+ *                 title:
+ *                   type: string
+ *                   example: blog a
+ *                 content:
+ *                   type: string
+ *                   example: content a
+ */
 app.post("/insert/single/data", insertApi.insertSingleData);
 
 // // Update single data into Elastic-search // indices students
