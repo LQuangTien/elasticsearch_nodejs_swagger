16b9d214d8b7736b95458dd7e2bbc54456fe9a70
VoHoangAn <vohoangan2000@gmail.com>
2022-04-24 16:15:00 +0700

    fix pagination

--- index.js
+++ index.js
@@ -23,11 +23,9 @@ app.use(express.static("public"));
 //     next();
 // });
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const server = require("http").createServer(app);
 const apiRoutes = require("./routes/routes");
